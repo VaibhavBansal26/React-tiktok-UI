863f4585dbdd96e1bdfa8518a7b9ad1e4fd04ba6
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2021-06-24 12:00:48 +0530

    phase 1 cont

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL:"https://mern-tiktok-vb.herokuapp.com/"
+    baseURL:"https://mern-tiktok-vb.herokuapp.com"
 });
 
 export default instance;
